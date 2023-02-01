59a457f8c88d724e45cb818116f1ce68ea70d84d
ctrahan93 <100620806+ctrahan93@users.noreply.github.com>
2023-02-01 18:10:06 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Cel_Trahan_WEB601Assignments_MyFavouriteVideoGames';
+}
